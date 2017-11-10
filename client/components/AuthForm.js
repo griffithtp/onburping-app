2547094930d20eb2fa3ea7ea38f6b388108c6483
Griffith Tchenpan <griffith.tchenpan@myworkpapers.com>
2017-11-10 16:06:49 +0000

    boilerplate project

---
+++ client/components/AuthForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {email: '', password: ''};
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state); // with the assumption this.state is always going to be {email, password}
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <form onSubmit={this.onSubmit.bind(this)} className="col s4">
+          <div className="input-field">
+            <input placeholder="Email"
+              value={this.state.emai}
+              onChange={e => this.setState({email: e.target.value})}
+            />
+          </div>
+          <div className="input-field">
+            <input type="password" placeholder="Password"
+              value={this.state.password}
+              onChange={e => this.setState({password: e.target.value})}
+            />
+          </div>
+
+          <div className="errors">
+            {this.props.errors.map(error => <div key={error}>{error}</div>)}
+          </div>
+
+          <button className="btn">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default AuthForm;
